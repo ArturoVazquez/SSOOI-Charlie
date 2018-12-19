0d4f83ed7c3ae1c0dc2da265e20bb18c3dcdf626
Rodrigo Zavala Zevallos <rodizz@hotmail.com>
2018-12-19 14:05:32 +0100

    Creación de todos los procesos
    Falta añadir señales y las mascaras

--- charlie.c
+++ charlie.c
@@ -17,16 +17,18 @@ typedef struct{
 	int num;
 
 } SOY;
+
 void dormirMalo (int );
 void dormirAngeles (int );
+int processBosley(SOY *);
+void processCharlie(SOY *);
+void processMalo(SOY *);
+void processAngel(SOY *, char *);
 
 int main (int argc, char* argv[], char * envp []){
-
     int rapido = 0;
-    pid_t charlie;
     SOY hijos;
-    
-    /* Entrada argumentos */
+
     if(argc>2){
 		printf("\nHaz introducido demasiados argumentos");
         return 1;
@@ -42,10 +44,26 @@ int main (int argc, char* argv[], char * envp []){
         }
     }
 
+    if (strcmp(argv[0],"./charlie") == 0) {
+        processCharlie(&hijos);
+    }else if(strcmp(argv[0],"bosley") == 0) {
+        processBosley(&hijos);
+    }else if(strcmp(argv[0],"sabrina") == 0 || strcmp(argv[0],"kelly") == 0 || strcmp(argv[0],"jill") == 0){
+        processAngel(&hijos, argv[0]);
+    }else if(strcmp(argv[0],"malo") == 0){
+        processMalo(&hijos);
+    }
+    return 0;
+}
+
+void processCharlie(SOY * hijos){
+    pid_t charlie;
     charlie = getpid();
+
+    printf("\nEJECUTANDO CHARLY...\n");
     printf("Soy Charlie : %d\n", charlie);
-    int status;
-    switch (hijos.bosley = fork())
+
+    switch (hijos->bosley = fork())
     {
         case -1:
             perror("\nNo se ha podido crear a Bosley\n");
@@ -53,55 +71,105 @@ int main (int argc, char* argv[], char * envp []){
             break;
         case 0:
             execl("./charlie","bosley", NULL);
-            printf("Soy Bosley : %d\n", getpid());
-            switch (hijos.sabrina = fork()){
+            perror("Error al crear al hijo ");
+            break;
+        default:
+            switch (hijos->malo = fork())
+            {
                 case -1:
-                    perror("\nNo se ha podido crear a Sabrina\n");
+                    perror("\nNo se ha podido crear a Bosley\n");
                     exit(1);
                     break;
                 case 0:
-                    execl("./charlie","sabrina", NULL);
-                    printf("Soy Sabrina : %d\n", getpid());
-                    break;
-                default:
-                    wait(NULL);
-                    switch (hijos.jill = fork()){
-                        case -1:
-                            perror("\nNo se ha podido crear a Jill\n");
-                            exit(1);
-                            break;
-                        case 0:
-                            execl("./charlie","jill", NULL);
-                            printf("Soy jill : %d\n", getpid());
-                            break;
-                        default:
-                            wait(NULL);
-                            switch (hijos.kelly = fork()){
-                                case -1:
-                                    perror("\nNo se ha podido crear a Kelly\n");
-                                    exit(1);
-                                    break;
-                                case 0:
-                                    execl("./charlie","kelly", NULL);
-                                    printf("Soy kelly : %d\n", getpid());
-                                    break;
-                                default:
-                                    wait(NULL);
-                                    printf("Soy el padre de kelly: %d\n",  getpid());
-                                    break;
-                            }
-                        break;
-                    }
+                    execl("./charlie","malo", NULL);
+                    perror("Error al crear al malo ");
                     break;
             }
-        break;     
-        default:
-            wait(NULL);
-            printf("Soy el padre : %d\n",  getpid());
-            break;
+        break;
     }
-    sleep(10);
-return 0;
+    sleep(90);
+    printf("\nFIN...\n");
+}
+
+int processBosley(SOY * hijos){
+    printf("\nEJECUTANDO BOSLEY...\n");
+    printf("Soy Bosley es %d\n", getpid());
+    
+    switch (hijos->sabrina = fork())
+    {
+        case -1:
+            perror("\nNo se ha podido crear a Sabrina\n");
+            exit(1);
+            break;
+        case 0:
+            execl("./charlie","sabrina", NULL);
+            perror("Error al crear al hijo ");
+            break;
+        default:
+        switch (hijos->jill = fork())
+        {
+            case -1:
+                perror("\nNo se ha podido crear a Bosley\n");
+                exit(1);
+                break;
+            case 0:
+                execl("./charlie","jill", NULL);
+                perror("Error al crear al hijo ");
+                break;
+            default:
+            switch (hijos->kelly = fork())
+            {
+                case -1:
+                    perror("\nNo se ha podido crear a Bosley\n");
+                    exit(1);
+                    break;
+                case 0:
+                    execl("./charlie","kelly", NULL);
+                    perror("Error al crear al hijo ");
+                    break;
+            }
+            sleep(10);
+            break;
+        }
+        sleep(10);
+        break;
+
+    }
+    printf("\nTODOS LOS ANGELES CREADOS\n");
+    sleep(20);
+}
+
+void processAngel(SOY * hijos, char *angel){
+    printf("\nEJECUTANDO ANGEL... %s\n", angel);
+    sleep(15);
+}
+
+void processMalo(SOY * hijos){
+    printf("\nEJECUTANDO MALO...\n");
+
+    int i=0;
+    hijos->malo = getpid();
+    printf("Soy Malo malisimo : %d\n",  getpid());
+    /*
+    while(i<20){  
+        dormirMalo(0);
+        switch (hijos->malo=fork()){
+            case -1:
+                perror("\nNo se ha podido crear reencarnacion del malo\n");
+                exit(1);
+                break;
+            case 0:
+                kill(hijos->malo, SIGKILL);
+                hijos->malo = getpid();
+                i++;
+                printf("JA, JA, JA, me acabo de reencarnar y mi nuevo PID es: %d. Que malo que soy...",hijos->malo);
+                break;
+        }
+     }
+     
+     if (i == 20) {
+         printf("\nHe sobrevivido a mi vigesima reencarnacion. Hago mutis por el foro\n");
+     }*/
 }
 
 void dormirAngeles (int velocidad){
